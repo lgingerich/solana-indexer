b41bcba25b39654772df0c1129bb4fc9a1b1e213
Landon Gingerich <landon.ging@gmail.com>
2024-09-19 19:20:48 -0500

    more cleanup

--- src/solana_indexer/utils.py
+++ src/solana_indexer/utils.py
@@ -1,13 +1,13 @@
-import logging
+import asyncio
 from colorlog import ColoredFormatter
+from functools import wraps
+import logging
 from logging.handlers import RotatingFileHandler
 import os
-import asyncio
-from functools import wraps
 import random
 from solana.exceptions import SolanaRpcException
 
-def setup_logger(log_file_path='logs/solana_indexer.log', log_level=logging.INFO):
+def setup_logger(log_file_path="logs/solana_indexer.log", log_level=logging.INFO):
     logger = logging.getLogger("main_logger")
     if not logger.handlers:
         logger.setLevel(log_level)
@@ -37,12 +37,17 @@ def setup_logger(log_file_path='logs/solana_indexer.log', log_level=logging.INFO
         if log_dir and not os.path.exists(log_dir):
             os.makedirs(log_dir)
 
-        file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(module)s - %(message)s')
-        file_handler = RotatingFileHandler(log_file_path, maxBytes=10*1024*1024, backupCount=5)  # 10MB file size
+        file_formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(module)s - %(message)s"
+        )
+        file_handler = RotatingFileHandler(
+            log_file_path, maxBytes=10 * 1024 * 1024, backupCount=5
+        )  # 10MB file size
         file_handler.setFormatter(file_formatter)
         logger.addHandler(file_handler)
 
     return logger
+
 
 # Create a single instance of the logger
 logger = setup_logger()
@@ -53,7 +58,7 @@ def async_retry(
     base_delay=1,
     exponential_backoff=True,
     jitter=True,
-    exceptions=(SolanaRpcException,)
+    exceptions=(SolanaRpcException,),
 ):
     def decorator(func):
         @wraps(func)
@@ -63,14 +68,24 @@ def async_retry(
                     return await func(*args, **kwargs)
                 except exceptions as e:
                     if attempt == retries:
-                        logger.error(f"All retry attempts failed for {func.__name__}: {str(e)}")
+                        logger.error(
+                            f"All retry attempts failed for {func.__name__}: {str(e)}"
+                        )
                         raise
-                    
-                    delay = base_delay * (2 ** (attempt - 1)) if exponential_backoff else base_delay
+
+                    delay = (
+                        base_delay * (2 ** (attempt - 1))
+                        if exponential_backoff
+                        else base_delay
+                    )
                     if jitter:
-                        delay *= (random.uniform(1.0, 1.5))
-                    
-                    logger.warning(f"Attempt {attempt} failed for {func.__name__}. Retrying in {delay:.2f} seconds. Error: {str(e)}")
+                        delay *= random.uniform(1.0, 1.5)
+
+                    logger.warning(
+                        f"Attempt {attempt} failed for {func.__name__}. Retrying in {delay:.2f} seconds. Error: {str(e)}"
+                    )
                     await asyncio.sleep(delay)
+
         return wrapper
+
     return decorator
