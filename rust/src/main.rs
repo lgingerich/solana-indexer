83778924062779418b15f69bb1f8b013fa0c25ab
Landon Gingerich <landon.ging@gmail.com>
2024-10-14 19:16:48 -0500

    working

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -10,9 +10,17 @@ const START_SLOT: Slot = 250_000_001;
 // const START_SLOT: Slot = 2_001;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let client = RpcClient::new(RPC_URL);
 
+    let block = process_block(client)?;
+
+    println!("Block: {:?}", block); // normal print
+    // println!("Block: {:#?}", block); // pretty print
+
+    Ok(())
+}
+
+fn process_block(client: RpcClient) -> Result<UiConfirmedBlock, Box<dyn std::error::Error>> {
     let config = solana_client::rpc_config::RpcBlockConfig {
         encoding: Some(solana_transaction_status::UiTransactionEncoding::Json),
         transaction_details: Some(solana_transaction_status::TransactionDetails::Full),
@@ -23,8 +31,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let block: UiConfirmedBlock = client.get_block_with_config(START_SLOT, config)?;
 
-    println!("Block: {:?}", block); // normal print
-    // println!("Block: {:#?}", block); // pretty print
-
-    Ok(())
+    Ok(block)
 }
